62586bf15c06af8fac146b9ac994129ad7cecd7f
Alexander Dmitriev <dmitriev@pdx.edu>
2018-01-27 18:16:49 -0800

    Brought back try/except blocks for error. Added msg for asserts.

--- tests/tester_csv_writer.py
+++ tests/tester_csv_writer.py
@@ -23,14 +23,17 @@ class TesterCsvWriter(TestCase):
         self.assertNotEqual(0, test_csv_write._client, "Class generated properly")
 
     def test_csv_file_to_data(self):
-        data_return_path = RESOURCES_DIR + "/" + 'temp2.csv'
-        initial_data_path = RESOURCES_DIR + "/" + 'temp.csv'
-        test_csv_write3 = CsvWriter(self.host, self.port, self.username, self.password, self.database)
-        test_csv_write3.csv_file_to_db()
-        test_csv_write3.data_to_csv_file('select * from per15min', new_csv_file_name=data_return_path)
-        self.assertTrue(self.compare_csv_files(data_return_path, initial_data_path),
-                        "Integration test failed, file is not the same")
-        os.remove(data_return_path)
+        try:
+            data_return_path = RESOURCES_DIR + "/" + 'temp2.csv'
+            initial_data_path = RESOURCES_DIR + "/" + 'temp.csv'
+            test_csv_write3 = CsvWriter(self.host, self.port, self.username, self.password, self.database)
+            test_csv_write3.csv_file_to_db()
+            test_csv_write3.data_to_csv_file('select * from per15min', new_csv_file_name=data_return_path)
+            self.assertTrue(self.compare_csv_files(data_return_path, initial_data_path),
+                            "Integration test failed, file is not the same")
+            os.remove(data_return_path)
+        except ConnectionError as error:
+            print("Test: Failed - {0}\n".format(error))
 
     def compare_csv_files(self, csv_a, csv_b):
         with open(csv_a, 'r') as t1, open(csv_b, 'r') as t2:
--- tests/tester_db_connector.py
+++ tests/tester_db_connector.py
@@ -18,14 +18,18 @@ class TesterDbConnector(TestCase):
     df = pd.DataFrame(data=list(range(30)), index=pd.date_range(start='2014-11-16', periods=30, freq='H'), columns=['0'])
 
     def test_connect_to_db(self):
-        test_db_connector_1 = InfluxDBConnector(self.username, self.password, self.port, self.database, self.host)
-        test_db_connector_1.create_database(self.database)
-        dict_test = {'name': self.database}
-        dbs = test_db_connector_1.list_databases()
-        self.assertTrue(dict_test in dbs)  # should be in the list
-        self.assertTrue(test_db_connector_1.write_points(self.df, 'adrian_test_db', protocol=self.protocol))
-        result = test_db_connector_1.query(self.query)
-        self.assertTrue(result)
-        test_db_connector_1.delete_database(self.database)
-        dbs = test_db_connector_1.list_databases()
-        self.assertTrue(dict_test not in dbs)  # should not be in the list
+        try:
+            test_db_connector_1 = InfluxDBConnector(self.username, self.password, self.port, self.database, self.host)
+            test_db_connector_1.create_database(self.database)
+            dict_test = {'name': self.database}
+            dbs = test_db_connector_1.list_databases()
+            self.assertTrue(dict_test in dbs, msg='database was not added')  # should be in the list
+            self.assertTrue(test_db_connector_1.write_points(self.df, 'adrian_test_db', protocol=self.protocol),
+                            msg='could not write points to database')
+            result = test_db_connector_1.query(self.query)
+            self.assertTrue(result, msg='nothing returned from query')
+            test_db_connector_1.delete_database(self.database)
+            dbs = test_db_connector_1.list_databases()
+            self.assertTrue(dict_test not in dbs, 'database still exists')  # should not be in the list
+        except ConnectionError as error:
+            print("Test: Failed - {0}\n".format(error))
