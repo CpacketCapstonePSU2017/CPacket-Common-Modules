2db408cdf3fe9db13d61915c1f9726d8a176e66e
Daniel Margason <daniel.margason@gmail.com>
2018-02-20 10:48:26 -0800

    This update changes the algorithm to drop and entire weeks worth of data if there is more than 5 consecutive days of missing data. This will keep all datasets consistent. Also added additional logic to take care of edge cases of needing to add datapoints to the end or beginning of a file

--- io_framework/csv_fill_data_gaps.py
+++ io_framework/csv_fill_data_gaps.py
@@ -7,48 +7,92 @@ all datapoints within that set.
 '''
 
 import pandas as pd
+import numpy as np
 import io_framework.csv_to_dataframe as cd
 
 
-def fill_data_gaps(file_path, output_file_path, num_data_points):
+def fill_data_gaps(file_path, num_data_points):
     '''
-
-    :param file_path: The path name for the time series csv file to be analysed
-    :param output_file_path: The folder path name where you want the final corrected csv file to be placed
+    :param file_path: The path name for the time series csv file to be analysed and replaced with corrected data
     :param num_data_points: total number of rows in the csv file being analysed
     '''
 
     # Skeleton dataframe that we will fill with the timestamps that need to be added
-    data_to_append = pd.DataFrame(data=None, columns=['','avg_hrcrx_max_byt'])
+    data_to_append = pd.DataFrame(data=None, columns=['', 'avg_hrcrx_max_byt'])
+
+    counter = 0
+    compute_start_fill = 1
+    compute_end_fill = 1
+    indexes_to_delete = np.array([])
+    indexes_to_delete_new_timestamp = np.array([])
 
     # Convert csv to dataframe and initialize for analysis
-    counter = 0
-    data = cd.csv_to_dataframe(file_path, 0, num_data_points, 0, ['',"avg_hrcrx_max_byt"])
+    data = cd.csv_to_dataframe(file_path, 0, num_data_points, 0, ['', "avg_hrcrx_max_byt"])
     data[''] = pd.to_datetime(data[''])
+    data_size = data.shape[0] - 1
 
     # Iterate through file and determine if there exists a gap > 15 min
-    for index, row in data.iterrows():
-        if counter < data.shape[0]-1:
-            min_diff = ((data[''][index+1] - data[''][index]).total_seconds() /60)
-            num_missing_periods = int(((min_diff / 15) - 1))
+    while counter <= data_size:
+        if compute_start_fill == 1 and counter == 0 and int((data[''][0] - data[''][0].normalize()).total_seconds() != 0):
+            min_diff = ((data[''][counter] - data[''][0].normalize()).total_seconds() / 60)
+        elif compute_end_fill == 1 and counter == data_size and int((((data[''][data_size].normalize() + pd.Timedelta(days=1)) + pd.Timedelta(minutes=-15)) - data[''][data_size]).total_seconds() != 0):
+            min_diff = (((data[''][data_size].normalize() + pd.Timedelta(days=1)) + pd.Timedelta(minutes=-15)) - data[''][counter]).total_seconds() / 60
+        elif counter == data_size:
+            min_diff = 15
+        else:
+            min_diff = ((data[''][counter + 1] - data[''][counter]).total_seconds() / 60)
 
-            # for every missing period, calculate the mean of surrounding datapoints
-            # and add the new period and calculated mean pair into skeleton dataframe
-            # from earlier
-            if num_missing_periods > 0:
-                new_row_time = pd.Timestamp(data[''][index])
-                new_byte_count = (data['avg_hrcrx_max_byt'][index+1] + data['avg_hrcrx_max_byt'][index])/2
-                while num_missing_periods > 0:
-                    new_row_time += pd.Timedelta(minutes=15)
-                    new_timestamp = pd.DataFrame({'': [new_row_time], 'avg_hrcrx_max_byt': [new_byte_count]})
-                    data_to_append = data_to_append.append(new_timestamp)
-                    num_missing_periods -= 1
-            counter += 1
+        num_missing_periods = int(((min_diff / 15) - 1))
+
+        # for every missing period, calculate the mean of surrounding datapoints
+        # and add the new period and calculated mean pair into skeleton dataframe
+        # from earlier
+        if 0 < num_missing_periods <= 5 and counter not in indexes_to_delete:
+            if compute_start_fill == 1 and counter == 0 and int((data[''][0] - data[''][0].normalize()).total_seconds() != 0):
+                num_missing_periods += 1
+                new_row_time = data[''][0].normalize() + pd.Timedelta(minutes=-15)
+            elif compute_end_fill == 1 and counter == data_size and int((((data[''][data_size].normalize() + pd.Timedelta(days=1)) + pd.Timedelta(minutes=-15)) - data[''][data_size]).total_seconds() != 0):
+                num_missing_periods += 1
+                new_row_time = pd.Timestamp(data[''][counter])
+            else:
+                new_row_time = pd.Timestamp(data[''][counter])
+
+            if compute_start_fill == 1 and counter == 0 and int((data[''][0] - data[''][0].normalize()).total_seconds() != 0):
+                new_byte_count = data['avg_hrcrx_max_byt'][counter]
+                counter -= 1
+                compute_start_fill = 0
+            elif compute_end_fill == 1 and counter == data_size and int((((data[''][data_size].normalize() + pd.Timedelta(days=1)) + pd.Timedelta(minutes=-15)) - data[''][data_size]).total_seconds() != 0):
+                new_byte_count = data['avg_hrcrx_max_byt'][counter]
+                compute_end_fill = 0
+            else:
+                new_byte_count = ((data['avg_hrcrx_max_byt'][counter + 1] + data['avg_hrcrx_max_byt'][counter]) / 2)
+
+            while num_missing_periods > 0:
+                new_row_time += pd.Timedelta(minutes=15)
+                new_timestamp = pd.DataFrame(
+                    {'': [new_row_time], 'avg_hrcrx_max_byt': [new_byte_count]})
+                data_to_append = data_to_append.append(new_timestamp)
+                data_to_append = data_to_append.reset_index(drop=True)
+                num_missing_periods -= 1
+        if num_missing_periods > 5 and counter not in indexes_to_delete:
+            day_of_week = data[''][counter].dayofweek
+            start_remove_date = data[''][counter].normalize() + pd.Timedelta(days=-day_of_week)
+            end_remove_date = data[''][counter].normalize() + pd.Timedelta(days=(7 - day_of_week))
+            x = data.index[(data[''] >= start_remove_date) and (data[''] < end_remove_date)]
+            y = data_to_append.index[
+                (data_to_append[''] >= start_remove_date) and (data_to_append[''] < end_remove_date)]
+            indexes_to_delete = np.append(x, indexes_to_delete)
+            indexes_to_delete_new_timestamp = np.append(y, indexes_to_delete_new_timestamp)
+        counter += 1
 
     # Merge the main dataset with the new timestamp dataset. Then order the combined
     # dataset by the timestamps
+    data = data.drop(labels=indexes_to_delete, axis=0)
+    data_to_append = data_to_append.drop(labels=indexes_to_delete_new_timestamp, axis=0)
     data = data.append(data_to_append)
     data = data.sort_values(by='', kind='mergesort')
+    data = data.reset_index(drop=True)
 
     # Output final corrected dataset to disk
-    data.to_csv(output_file_path+'dummy.csv', columns=['', 'avg_hrcrx_max_byt'], index=False)
+    data.to_csv(file_path, columns=['', 'avg_hrcrx_max_byt'],
+                index=False)
